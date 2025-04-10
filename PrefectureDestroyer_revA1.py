dd08aa20cd8d367979eb9ca3b1e27d41f465d66d
Viet Phuong DINH <vietphuong.dinh@se.com>
2025-04-10 16:28:28 +0200

    test

--- PrefectureDestroyer_revA1.py
+++ PrefectureDestroyer_revA1.py
@@ -20,7 +20,7 @@ async def notif(title, message):
 # In[171]:
 
 notifier = DesktopNotifier()
-
+#test 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--disable-extensions")
